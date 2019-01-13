ed04595f5b61ac07a246f0894d5132797a4da04e
vasu jain <vasujain00@users.noreply.github.com>
2019-01-13 12:41:34 -0500

    Train Test Split
    Updated Code
    Data is splitted
    Linear Regression is being applied on data to predict 10 days future value
    96% accuracy is being achieved right now

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import quandl
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 #print(df.head())
@@ -13,4 +17,30 @@ df['PCT_CHANGE'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 
 df = df[['Adj. Open','HL_PCT','PCT_CHANGE','Adj. Volume']]
 
-print(df.head())
+forecast_col = 'Adj. Close'
+
+df.fillna(-99999,inplace=True)
+
+forecast_out = int(math.ceil(0.1*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
+
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train,y_train)
+accuracy = clf.score(X_test,y_test)
+
+print(accuracy)
+
+
+
+
